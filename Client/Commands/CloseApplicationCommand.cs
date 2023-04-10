feed875bdc36aa5f112d588f1a8b01418b907379
Nikolay Makalov <nexterrible@mail.ru>
2023-04-10 16:09:02 +0700

    CloseApplicationCommand

---
+++ Client/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Client.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            App.Current.Shutdown();
+        }
+    }
+}
